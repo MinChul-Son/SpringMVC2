1cdd58b00363ce536bb01846feda9ae78e8e55b1
MinChul-Son <smc5236@naver.com>
2021-07-19 22:37:27 +0900

    쿠키, 세션 : 세션 직접만들기
    세션에는 세션 생성, 세션 조회, 세션 만료 기능이 필요하다.
    이를 직접 만들어보고 테스트코드를 통해 검증해보았다.

---
+++ Effective-Java/src/Item1.java
@@ -0,0 +1,14 @@
+import java.util.Collections;
+
+public class Item1 {
+    String name;
+    String address;
+
+    public Item1(String name) {
+        this.name = name;
+    }
+
+//    public Item1(String address) { // 같은 시그니처 생성자 불가능
+//        this.address = address;
+//    }
+}
